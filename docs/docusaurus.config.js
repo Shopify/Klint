a4faea754f5685307b6f8299b3806c315bede29a
arthurcloche <arthur.cloche@shopify.com>
2025-03-09 22:33:13 -0400

    feat : edit docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,19 +1,21 @@
 import { themes as prismThemes } from "prism-react-renderer";
-
 /** @type {import('@docusaurus/types').Config} */
+/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+
 const config = {
   title: "Klint Handbook",
-  tagline: "A Modern 2D Canvas",
-  favicon: "img/favicon.ico",
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  tagline: "The manual to a modern 2D Canvas made for React",
+  url: "https://shopify.com",
   baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Shopify",
+  projectName: "klint",
 
   // GitHub pages deployment config.
   organizationName: "Shopify",
   projectName: "Klint",
-
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
@@ -30,8 +32,6 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
@@ -41,87 +41,189 @@ const config = {
       }),
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
-      navbar: {
-        title: "Klint",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
+  plugins: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        docsDir: "docs",
+        indexPages: true,
+        docsRouteBasePath: "/",
+      },
+    ],
+  ],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@shopify/docusaurus-docuchat", "@docusaurus/theme-mermaid"],
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Klint",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "sidebar",
+          position: "left",
+          label: "Docs",
         },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "sidebar",
-            position: "left",
-            label: "Docs",
-          },
-          {
-            href: "https://github.com/Shopify/Klint",
-            label: "Editor",
-            position: "left",
-          },
-          {
-            href: "https://github.com/Shopify/Klint",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "X",
-                href: "https://x.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Shopify. Made w/ 💚 for Shopifolks, hit me @ac on Slack.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+        {
+          href: "https://github.com/Shopify/Klint",
+          label: "Editor",
+          position: "left",
+        },
+        {
+          href: "https://github.com/Shopify/Klint",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Slack",
+              href: "https://shopify.enterprise.slack.com/archives/C04UJANU3LP",
+            },
+            {
+              label: "Vault",
+              href: "https://shopify.com",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/shopify/Klint",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Shopify Inc. Made w/ 💚 for Shopifolks, hit me @ac on Slack.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 };
 
 export default config;
+/*
+module.exports = {
+  title: "Klint Handbook",
+  tagline: "The manual to a modern 2D Canvas made for React",
+  url: "https://shopify.com",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Shopify",
+  projectName: "klint",
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Klint",
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "sidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/Shopify/Klint",
+          label: "Editor",
+          position: "left",
+        },
+        {
+          href: "https://github.com/Shopify/Klint",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Slack",
+              href: "https://shopify.enterprise.slack.com/archives/C04UJANU3LP",
+            },
+            {
+              label: "Vault",
+              href: "https://shopify.com",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/shopify/Klint",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Shopify Inc. Made w/ 💚 for Shopifolks, hit me @ac on Slack.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
+  plugins: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        docsDir: "docs",
+        indexPages: true,
+        docsRouteBasePath: "/",
+      },
+    ],
+  ],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@shopify/docusaurus-docuchat", "@docusaurus/theme-mermaid"],
+  // presets: [
+  //   [
+  //     "@docusaurus/preset-classic",
+  //     {
+  //       docs: {
+  //         showLastUpdateAuthor: true,
+  //         showLastUpdateTime: true,
+  //         path: "docs",
+  //         routeBasePath: "/",
+  //         sidebarPath: require.resolve("./sidebars.js"),
+  //         editUrl: "https://github.com/shopify/three/edit/main/docusaurus/",
+  //       },
+  //       blog: false,
+  //       pages: false,
+  //       sitemap: false,
+  //       theme: {
+  //         customCss: ["./src/css/custom.css"],
+  //       },
+  //     },
+  //   ],
+  // ],
+};
+*/
