b9314446c59b882f22d2cdfcef14536ae40a228c
arthurcloche <arthur.cloche@shopify.com>
2025-03-09 20:11:27 -0400

    feat : docs : testing structure

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,36 +1,23 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import { themes as prismThemes } from "prism-react-renderer";
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Klint Handbook",
+  tagline: "A Modern 2D Canvas",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "Shopify",
+  projectName: "Klint",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  // locales
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -69,7 +56,7 @@ const config = {
         items: [
           {
             type: "docSidebar",
-            sidebarId: "tutorialSidebar",
+            sidebarId: "sidebar",
             position: "left",
             label: "Docs",
           },
@@ -128,7 +115,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Shopify. Made w/ 💚 for Shopifolks, hit me @ac on Slack.`,
       },
       prism: {
         theme: prismThemes.github,
