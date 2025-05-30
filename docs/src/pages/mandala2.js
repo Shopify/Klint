535e2397f8863a849b99e5482a06251dee824968
CarolynMcNeillie <carolyn.mcneillie@shopify.com>
2025-05-30 09:58:44 -0400

    mandela

---
+++ docs/src/pages/mandala2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Mandala2 from "@site/src/components/Experiments/Mandala2";
+
+export default function MandalaPage() {
+  return (
+    <main className="h-screen w-screen">
+      <Mandala2 />
+    </main>
+  );
+}
