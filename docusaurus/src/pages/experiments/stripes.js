e5ef4ae83b12be248cece17651e708bd469e8ab1
CarolynMcNeillie <carolyn.mcneillie@shopify.com>
2025-06-01 09:47:25 -0400

    Josh experiment

---
+++ docusaurus/src/pages/experiments/stripes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Stripes from "@site/src/components/Experiments/Stripes";
+
+export default function StripesPage() {
+  return (
+    <main className="h-screen w-screen">
+      <Stripes />
+    </main>
+  );
+}
