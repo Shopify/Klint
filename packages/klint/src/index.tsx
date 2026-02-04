0eb36921d9c9ef92860439f850dbadb8a884d7fa
arthurcloche <arthur.cloche@shopify.com>
2026-02-04 12:48:29 -0500

    feat : update release workflow for OICD

--- packages/klint/src/index.tsx
+++ packages/klint/src/index.tsx
@@ -3,6 +3,7 @@ import useKlint, { useProps, useStorage } from "./useKlint";
 import { type KlintMouse, type KlintScroll } from "./useKlint";
 
 import { KlintCoreFunctions, KlintFunctions } from "./KlintFunctions";
+import { type KlintPerformanceMetrics } from "./Klint";
 
 export * from "./Klint";
 export * from "./elements";
